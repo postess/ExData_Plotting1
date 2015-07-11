790b0c6c0fcd4add4daadbe0776023a62645a3fd
Sharon Post <postess@users.noreply.github.com>
2015-07-11 10:59:32 -0500

    Update plot4.R
    adds png() call, which fixed legend (h/t Ilya Kulyatin)

--- plot4.R
+++ plot4.R
@@ -17,6 +17,8 @@ dtCol <- dmy_hms(dt)
 energyDF$DateTime <- dtCol
 
 ## plot the four graphs from plot 4
+png(file = "plot4.png", width = 480, height = 480)
+
 par(mfrow = c(2,2), mar = rep(4,4), oma = c(4, 4, 0, 0))
 with(energyDF, {
         plot(DateTime, Global_active_power, type = "l", ylab = "Global Active Power", xlab = "")
@@ -25,7 +27,9 @@ with(energyDF, {
         plot(x = DateTime, y = Sub_metering_1, type = "l", ylab= "Energy sub metering", xlab = "")
                 lines(DateTime, Sub_metering_2, col ="red")
                 lines(DateTime, Sub_metering_3, col ="blue")
-                legend("topright", col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = c(1,1,1), bty = "n", cex = .35)
+                legend("topright", col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = c(1,1,1), bty = "n", cex = .65)
 }
         plot(DateTime, Global_reactive_power, type = "l", ylab = "Global_reactive_power", xlab = "datetime")
 })
+
+dev.off()
