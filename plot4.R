6e6f3de7b43033d92da853a917de305f32681e98
Sharon Post <postess@users.noreply.github.com>
2015-07-11 08:38:01 -0500

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -1,3 +1,22 @@
+## load data only for February 1 & 2, 2007
+url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+destination <-"./data/household_power_consumption.zip"
+download.file(url, destination, method = "curl")
+energyDF <- read.table(unz(destination, "household_power_consumption.txt"), sep = ";", na.strings = "?", skip = 66637, nrows = 2880)
+## from Astrego on How to read zipped data from the Coursera discussion forum thread
+
+## skipped headers when loading, so add back column header names
+colHeadings <- c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
+colnames(energyDF) <- colHeadings
+
+## change date and time classes and create one DateTime column
+library(lubridate)
+dt <- paste(energyDF$Date, energyDF$Time, sep = " ")
+dtCol <- dmy_hms(dt)
+
+energyDF$DateTime <- dtCol
+
+## plot the four graphs from plot 4
 par(mfrow = c(2,2), mar = rep(4,4), oma = c(4, 4, 0, 0))
 with(energyDF, {
         plot(DateTime, Global_active_power, type = "l", ylab = "Global Active Power", xlab = "")
